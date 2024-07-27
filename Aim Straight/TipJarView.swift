ce2dec79aa1f634219018b0931280030599b9382
lardieri <lardieri@users.noreply.github.com>
2024-07-26 19:10:09 -0700

    Add a title, call to action, and close button to the Tip Jar view.

--- Aim Straight/TipJarView.swift
+++ Aim Straight/TipJarView.swift
@@ -9,10 +9,60 @@ import SwiftUI
 import StoreKit
 
 struct TipJarView: View {
+    @Environment(\.dismiss) private var dismiss
     let productIdentifiers: [String]
 
     var body: some View {
+        VStack(alignment: .center, spacing: 20.0) {
+            closeButton()
+            titleView()
+            ctaView()
+            storeView()
+        }
+        .background(.ultraThickMaterial, in: .rect(cornerRadius: 20))
+    }
+
+    @ViewBuilder
+    private func closeButton() -> some View {
+        HStack {
+            Color.clear
+                .frame(maxWidth: .infinity, maxHeight: 44.0)
+            Image(systemName: "xmark")
+                .frame(width: 44.0, height: 44.0)
+                .onTapGesture {
+                    dismiss()
+                }
+        }
+    }
+
+    @ViewBuilder
+    private func titleView() -> some View {
+        HStack {
+            Text(" ")
+            Text("Tip Jar")
+            Text(" ")
+            ZStack {
+                Text("🫙")
+                Text("❤️")
+                    .font(.footnote)
+            }
+        }
+            .font(.largeTitle)
+            .fontWeight(.heavy)
+    }
+
+    @ViewBuilder
+    private func ctaView() -> some View {
+        let appName = (Bundle.main.infoDictionary?["CFBundleName"] as? String) ?? "Aim Straight"
+        let message = "If you are enjoying \(appName), you may send a gratuity to the developer."
+        Text(message)
+            .padding()
+    }
+
+    @ViewBuilder
+    private func storeView() -> some View {
         StoreView(ids: productIdentifiers)
+        .storeButton(.hidden, for: .cancellation)
     }
 }
 
